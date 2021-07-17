9d13556976d0bed5713149188af07e51897c0878
willyrgf <willyr.goncalves@gmail.com>
2021-07-17 13:43:41 -0300

    connect in binance ws and print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use binance::websockets::*;
+use std::sync::atomic::{AtomicBool, Ordering};
+
 fn main() {
-    println!("Hello, world!");
+    let keep_running = AtomicBool::new(true);
+    let agg_trade = format!("!ticker@arr");
+    let mut web_socket: WebSockets = WebSockets::new(|event: WebsocketEvent| {
+        match event {
+            WebsocketEvent::DayTickerAll(ticker_events) => {
+                for tick_event in ticker_events {
+                    println!("{:?}", tick_event);
+                }
+            }
+            _ => (),
+        };
+
+        Ok(())
+    });
+
+    match web_socket.connect(agg_trade.as_str()) {
+        Ok(_) => match web_socket.event_loop(&keep_running) {
+            Err(err) => println!("event_loop Error: {:?}", err),
+            _ => (),
+        },
+
+        Err(e) => println!("connect Error: {:?}", e),
+    };
 }
