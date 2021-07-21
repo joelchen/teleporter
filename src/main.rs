eb4d94b7062ead59fcfd1ca31e30c0b65623ec59
willyrgf <willyr.goncalves@gmail.com>
2021-07-20 22:34:00 -0300

    create a default init of market_ticker in datastore

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use binance::websockets::*;
-use std::sync::atomic::AtomicBool;
+use std::{sync::atomic::AtomicBool, thread, time::Duration};
+use teleporter::{create_market_ticker, establish_connection, models::MarketTicker};
 
 fn main() {
     let keep_running = AtomicBool::new(true);
     let agg_trade = format!("!ticker@arr");
+    let conn = establish_connection();
+    let t = MarketTicker::default();
+    create_market_ticker(&conn, &t);
     let mut web_socket: WebSockets = WebSockets::new(|event: WebsocketEvent| {
         match event {
             WebsocketEvent::DayTickerAll(ticker_events) => {
                 for tick_event in ticker_events {
                     println!("{:?}", tick_event);
+                    thread::sleep(Duration::from_secs(1))
                 }
             }
             _ => (),
