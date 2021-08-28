6d3cb52fc6347cb7469899fdc815f8875237dfca
willyrgf <willyr.goncalves@gmail.com>
2021-08-28 18:16:28 -0300

    show tokio result

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use tokio::task;
 #[tokio::main]
 async fn main() {
     let http_server = task::spawn(start_http_server());
-    let websocket_client= task::spawn(start_websocket_client());
+    let websocket_client = task::spawn(start_websocket_client());
 
-    let _ = tokio::join!(http_server, websocket_client);
+    let result = tokio::join!(http_server, websocket_client);
+    println!("result: {:?}", result)
 }
